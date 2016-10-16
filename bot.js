3df87ba66dea8da9dd2fc7bdfe2dc69c6888f03f
Dusan Stankovic <dusanstankovic90@gmail.com>
2016-10-16 05:40:27 +0200

    pay

--- bot.js
+++ bot.js
@@ -6,32 +6,63 @@ var mBills = require('./mBills.js');
 
 module.exports = bot(function(request, originalApiRequest) {
 
-	if (request.text === "DAILY_MENU") {
-		return getItems().get();
-	} else if (request.text.indexOf("ORDER") === 0) {
-		return "You will be notified when order is payed."
-	} else if (request.text === "EXIT") {
-		return "Have a nice day";
 
-	} else if (request.text === ".") {
-		return mBills.pay(100).then(function(token) {
-			return new fbTemplate.button("Button will open external app for payment purposes")
-				.addButton("Confirm purchase", "http://dulerock.com/fintech-adria/?token=" + token + "&amount=100").get()
-		});
-	} else if (request.text === "h") {
-		return new fbTemplate.button("Item is payed, want me to drop it?")
-			.addButton("Yep yep", "DROP").get()
-	} else if (request.text === "DROP") {
-		return rp.get("http://gsiot-egp6-3b0j.try.yaler.io").then(function() {
-			return "bon appétit";
-		});
-	} else {
-		return ["Hi, I'm Tomy Samwich, the guy who sells sandwiches", ,
-			new fbTemplate.button("Would you like to eat?")
-			.addButton("Yeah, I'm starving!", "DAILY_MENU")
-			.addButton("Not now, thanks", "EXIT").get()
-		];
+	switch (request.text) {
+		case "DAILY_MENU":
+			{
+				return getItems().get();
+			}
+		case "ORDER":
+			{
+				return "You will be notified when order is payed."
+			}
+		case ".":
+			{
+				return mBills.pay(100).then(function(token) {
+					return new fbTemplate.generic()
+						.addBubble('Classic sandwich - 2.50€')
+						.addImage("https://raw.githubusercontent.com/dstankovic/fintech-adria/master/assets/img/classic.png")
+						.addButton('Pay for the item(s)', "http://dulerock.com/fintech-adria/?amount=100&token=" + token).get();
+				});
+			}
+		case "EXIT":
+			{
+				return "Have a nice day";
+			}
+		case "DONE":
+			{
+				return new fbTemplate.receipt("Carefull Mom", "12345678902", "USD", "Visa 2345")
+					.addTimestamp(new Date(1428444852))
+					.addItem("Classic sandwich")
+					.addSubtitle("Juicy slices of ham with slices of cheese and pickles")
+					.addQuantity(1)
+					.addPrice(2.50)
+					.addCurrency("EUR")
+					.addImage("https://raw.githubusercontent.com/dstankovic/fintech-adria/master/assets/img/classic.png")
+					.addTotal(2.50)
+					.get();
+			}
+		case "DROP":
+			{
+				return rp.get("http://gsiot-egp6-3b0j.try.yaler.io").then(function() {
+					return "bon appétit";
+				});
+			}
+		case "h":
+			{
+				return new fbTemplate.button("Item is payed, want me to drop it?")
+					.addButton("Yep yep", "DROP").get()
+			}
+		default:
+			{
+				return ["Hi, I'm Tomy Samwich, the guy who sells sandwiches", ,
+					new fbTemplate.button("Would you like to eat?")
+					.addButton("Yeah, I'm starving!", "DAILY_MENU")
+					.addButton("Not now, thanks", "EXIT").get()
+				];
+			}
 	}
+
 
 });
 
@@ -40,7 +71,7 @@ function getItems() {
 	for (var i = 0; i < inventory.length; i++) {
 		template.addBubble(inventory[i].name + ' - ' + inventory[i].price + '€')
 			.addImage(inventory[i].image)
-			.addButton('I want this!', 'ORDER' + i);
+			.addButton('I want this!', 'ORDER');
 	}
 	return template;
 }
